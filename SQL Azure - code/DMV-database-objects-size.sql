74e7669f43464af505cb3612ca06ad0162dfb8fb
SoftwareONE-chile <40842456+SoftwareONE-chile@users.noreply.github.com>
2018-07-04 22:55:24 -0400

    sql azure database objects DMV

---
+++ SQL Azure - code/DMV-database-objects-size.sql
@@ -0,0 +1,7 @@
+
+-- Calcula el tamaño individual de los objetos de la base de datos
+SELECT sys.objects.name, SUM(reserved_page_count) * 8.0 / 1024
+FROM sys.dm_db_partition_stats, sys.objects
+WHERE sys.dm_db_partition_stats.object_id = sys.objects.object_id
+GROUP BY sys.objects.name;
+GO
